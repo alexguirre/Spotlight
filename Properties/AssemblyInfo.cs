3741358f85b7561aece088f7d4b2d7d724e2729f
alexguirre <alexguirre.aa@gmail.com>
2017-05-09 22:24:52 +0200

    Update version attribute

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using Rage.Attributes;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3ffad4ee-304c-47e6-8bdc-1dd20b9c6385")]
-[assembly: AssemblyVersion("0.0.1.*")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: Plugin("Spotlight", Author = "alexguirre", PrefersSingleInstance = true)]
